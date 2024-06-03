6f84fb06967a6775d31e59f9e5c0f0cbd8561bbd
Fehmmi Aliti <fehmmialiti@gmail.com>
2024-06-03 10:27:38 +0200

    added secrets..

--- src/config/firebase_config.js
+++ src/config/firebase_config.js
@@ -1,28 +1,34 @@
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import { getFirestore } from '@firebase/firestore';
 
 
-import {
-  VITE_API_KEY,
-  VITE_APP_ID,
-  VITE_AUTH_DOMAIN,
-  VITE_MESSAGING_SENDER_ID,
-  VITE_PROJECT_ID,
-  VITE_STORAGE_BUCKET,
-} from './env';
+// import {
+//   VITE_API_KEY,
+//   VITE_APP_ID,
+//   VITE_AUTH_DOMAIN,
+//   VITE_MESSAGING_SENDER_ID,
+//   VITE_PROJECT_ID,
+//   VITE_STORAGE_BUCKET,
+// } from './env';
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: VITE_API_KEY,
+//   authDomain: VITE_APP_ID,
+//   projectId: VITE_AUTH_DOMAIN,
+//   storageBucket: VITE_MESSAGING_SENDER_ID,
+//   messagingSenderId: VITE_PROJECT_ID,
+//   appId: VITE_STORAGE_BUCKET
+// };
+
 const firebaseConfig = {
-  apiKey: VITE_API_KEY,
-  authDomain: VITE_APP_ID,
-  projectId: VITE_AUTH_DOMAIN,
-  storageBucket: VITE_MESSAGING_SENDER_ID,
-  messagingSenderId: VITE_PROJECT_ID,
-  appId: VITE_STORAGE_BUCKET
+  apiKey: "AIzaSyAxbWXpxLsGmGnSBuCEG62OM79IYPqyNXY",
+  authDomain: "recycle-bottles-project.firebaseapp.com",
+  projectId: "recycle-bottles-project",
+  storageBucket: "recycle-bottles-project.appspot.com",
+  messagingSenderId: "37376978764",
+  appId: "1:37376978764:web:10490a5896ca56fbc6a9e9"
 };
 
 // Initialize Firebase
